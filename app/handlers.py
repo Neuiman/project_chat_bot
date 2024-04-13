0af98f0b2481344c81c488ba761f3089bd3552f7
Mark <marat.bakirov,1998@gmail.com>
2024-04-13 17:11:35 +0500

    add history_clean func

--- app/handlers.py
+++ app/handlers.py
@@ -1,7 +1,7 @@
 from aiogram import F, Router, Bot
 from aiogram.types import Message
 from aiogram.filters import Command
-
+from aiogram.exceptions import TelegramBadRequest
 router = Router()
 
 
@@ -13,4 +13,14 @@ router = Router()
 @router.message(Command('start'))
 async def start_command(message: Message):
     await message.answer(f'Привет, {message.from_user.first_name},'
-                        f' ты попал в бота, интрегрированного с системой управления проектами')
+                         f' ты попал в бота, интрегрированного с системой управления проектами')
+
+
+@router.message(Command('clear'))
+async def clear_command(message: Message, bot: Bot):
+    try:
+        for i in range(message.message_id, 0, -1):
+            await bot.delete_message(message.chat.id, i)
+    except TelegramBadRequest as ex:
+        if ex.message == 'Bad Request: message to delete not found':
+            print("Щеф, я все удалить!")
